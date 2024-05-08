d66f55ce75174320ec29a59a56e9f959700c72d3
Fabriziogianni7 <fabrizio@keyko.io>
2024-05-08 08:04:24 +0200

    contracts - poolFactory teleport function

--- src/CrossChainPool.sol
+++ src/CrossChainPool.sol
@@ -23,10 +23,15 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.25;
 
+import {console2} from "forge-std/Test.sol";
+
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import {SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import {ReentrancyGuard} from "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
+import {CCIPReceiver} from "ccip/applications/CCIPReceiver.sol";
+import {Client} from "ccip/libraries/Client.sol";
+import {IRouterClient} from "ccip/interfaces/IRouterClient.sol";
 
 /**
  * @notice
@@ -50,24 +55,47 @@ import {ReentrancyGuard} from "@openzeppelin/contracts/utils/ReentrancyGuard.sol
  *  @custom:interfaces
  *     inherit from CCIP contracts and functions contracts
  */
-contract CrossChainPool is ERC20, ReentrancyGuard {
+contract CrossChainPool is ERC20, ReentrancyGuard, CCIPReceiver {
     // errors
     error CrossChainPool__ShouldBeMoreThanZero();
     error CrossChainPool__WrongUnderlying();
     error CrossChainPool__NotLP();
+    error CrossChainPool__SenderOrSelectorNotAllowed();
+    error CrossChainPool__UserFeesNotEnough();
     // interfaces, libraries, contracts
+
     // Type declarations
-
     using SafeERC20 for IERC20;
+
     // State variables
-
     IERC20 private immutable i_underlyingToken;
-    // Events
+    IERC20 private immutable i_otherChainUnderlyingToken;
+    uint64 private immutable i_crossChainSelector;
+    address private s_crossChainPool;
+    bytes32 private s_lastSentMessageId;
+    bytes32 private s_lastReceivedMessageId; // Store the last received messageId.
+    mapping(address => uint256) private readyToWithdraw;
 
+    uint8 private constant TELEPORT_FUNCTION_ID = 1;
+    uint8 private constant TELEPORT_SUCCESS_FUNCTION_ID = 2;
+    uint8 private constant TELEPORT_FAIL_FUNCTION_ID = 3;
+
+    // Events
     event DepositedAndMintedLpt(address indexed lp, uint256 indexed lptAmount, uint256 indexed underlyingAmount);
     event Redeemed(address indexed lp, uint256 indexed lptAmountBurnt, uint256 indexed underlyingAmount);
+    event TeleportStarted(uint256 indexed value, address indexed to);
+    event MessageReceived(bytes32 indexed messageId);
 
     // Modifiers
+    modifier areSenderAndSelectorAllowed(address _sender, uint64 _selector) {
+        (address allowedSender, uint64 allowedSelector) = getCrossChainSenderAndSelector();
+
+        if (allowedSender != _sender || allowedSelector != _selector) {
+            revert CrossChainPool__SenderOrSelectorNotAllowed();
+        }
+
+        _;
+    }
 
     modifier moreThanZero(uint256 amount) {
         if (amount <= 0) {
@@ -92,14 +120,31 @@ contract CrossChainPool is ERC20, ReentrancyGuard {
     // Functions
 
     // @todo the ERC20 should have a better name specifying the networks
-    constructor(IERC20 _underlyingToken, string memory _name) ERC20(_name, "LPT") {
-        i_underlyingToken = _underlyingToken;
+    /**
+     * @notice instantiate the pool
+     * @param _underlyingToken is the ERC20 that gets teleported
+     * @param _name is the name of this pool
+     * @param _crossChainSelector is the corresponding pool selector on the other network
+     * @param _router the router of the current network
+     * @param _otherChainUnderlyingToken the other token of the pair
+     */
+    constructor(
+        address _underlyingToken,
+        string memory _name,
+        uint64 _crossChainSelector,
+        address _router,
+        address _otherChainUnderlyingToken
+    ) ERC20(_name, "LPT") CCIPReceiver(_router) {
+        i_underlyingToken = IERC20(_underlyingToken);
+        i_otherChainUnderlyingToken = IERC20(_otherChainUnderlyingToken);
+        i_crossChainSelector = _crossChainSelector;
     }
 
     // external
     /**
      * @notice mint LPTs to LPs and get the underlyng in exchange
      * @notice follow CEI pattern Cecks Effects Interactions
+     * @notice LPs cannot deposit crosschain -> they need to switch network if they want to deposit in the other pool
      *  @param _token the token to deposit as underlying
      *  @param _amount the _amount to deposit as underlying
      */
@@ -131,8 +176,34 @@ contract CrossChainPool is ERC20, ReentrancyGuard {
         // burn lp
         _burn(msg.sender, _lptAmount);
     }
-    // bridgeTo() --> send a ccip message on other chain
-    // getBridgedToken() --> send tokens bridged from th other network to user, receive a message from ccip (maybe internal)
+
+    /**
+     * @notice add the crosschain sender of the other pool
+     * todo add access control
+     */
+    function addCrossChainSender(address _sender) external {
+        s_crossChainPool = _sender;
+    }
+
+    /**
+     * @notice users deposit here underlying token and send a message to the other pool crosschain to make the teleportation of the tokens
+     *  @param _value the amount to teleport
+     *  @param _to the address to send token on destination chain
+     */
+    function startTeleport(uint256 _value, address _to) external payable {
+        address router = getRouter();
+
+        // at this point this contract should have enough money to pay fee
+
+        // user deposit underlying tokens
+        emit TeleportStarted(_value, _to);
+        // todo calculate fees and add it to value
+        i_underlyingToken.safeTransferFrom(msg.sender, address(this), _value);
+        // send a message to other pool
+        _sendCCipMessageTeleport(_value, _to, router);
+    }
+
+    // function teleportTransfer() external {} --> send tokens bridged from th other network to user, receive a message from ccip (maybe internal)
 
     // public
     /**
@@ -170,10 +241,79 @@ contract CrossChainPool is ERC20, ReentrancyGuard {
     }
 
     // internal
+    function _sendCCipMessageTeleport(uint256 _value, address _to, address _router) internal {
+        Client.EVM2AnyMessage memory message = _buildTeleportMessage(_value, _to);
+
+        uint256 fee = IRouterClient(_router).getFee(i_crossChainSelector, message);
+        if (msg.value < fee) {
+            revert CrossChainPool__UserFeesNotEnough();
+        }
+
+        s_lastSentMessageId = IRouterClient(_router).ccipSend{value: msg.value}(i_crossChainSelector, message);
+    }
+
+    function _buildTeleportMessage(uint256 _value, address _to)
+        internal
+        view
+        returns (Client.EVM2AnyMessage memory message)
+    {
+        message = Client.EVM2AnyMessage({
+            receiver: abi.encode(s_crossChainPool), // receiver is the pool on destination chain
+            data: abi.encode(TELEPORT_FUNCTION_ID, _value, _to), //  the parameters to pass into deployPool
+            tokenAmounts: new Client.EVMTokenAmount[](0), // we are not passing tokens even tho we bridge bc we cool AF
+            extraArgs: Client._argsToBytes(
+                // Additional arguments, setting gas limit
+                Client.EVMExtraArgsV1({gasLimit: 3_000_000})
+            ),
+            feeToken: address(0) // will pay with eth
+        });
+    }
+
+    function _simulateMessageTeleport(uint256 _value, address _to)
+        internal
+        view
+        returns (Client.EVM2AnyMessage memory message)
+    {
+        message = _buildTeleportMessage(_value, _to);
+    }
+
+    function _ccipReceive(Client.Any2EVMMessage memory any2EvmMessage) internal override {
+        s_lastReceivedMessageId = any2EvmMessage.messageId;
+
+        (uint8 functionID, uint256 value, address to) = abi.decode(any2EvmMessage.data, (uint8, uint256, address));
+
+        if (functionID == TELEPORT_FUNCTION_ID) {
+            // add
+            console2.log("i_underlyingToken", address(i_underlyingToken));
+            i_underlyingToken.safeTransfer(to, value); //todo review this
+            console2.log("balance after transfer", i_underlyingToken.balanceOf(to));
+
+            // sending  positive ack if transfer is success todo how do I pay for it?
+        }
+        emit MessageReceived(any2EvmMessage.messageId);
+    }
     // isEnoughBalanceOnOtherChain() --> check pending ccip messages and subtract total amount pending from total amount on pool contract ()on other chain)
 
     // view fucntions
     function getUnderlyingToken() external view returns (IERC20) {
         return i_underlyingToken;
+    }
+
+    function getCrossChainSenderAndSelector() public view returns (address, uint64) {
+        return (s_crossChainPool, i_crossChainSelector);
+    }
+
+    function getOtherChainUnderlyingToken() public view returns (address) {
+        return address(i_otherChainUnderlyingToken);
+    }
+
+    /**
+     * @notice this is used with startTeleport function to forecast the fees and let the user pay those fees
+     *  todo add the fees for the liquidity provider later on
+     */
+    function getFeesForTeleporting(uint256 _value, address _to) public view returns (uint256) {
+        address router = getRouter();
+        Client.EVM2AnyMessage memory message = _simulateMessageTeleport(_value, _to);
+        return IRouterClient(router).getFee(i_crossChainSelector, message);
     }
 }
--- src/PoolFactory.sol
+++ src/PoolFactory.sol
@@ -23,6 +23,8 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.25;
 
+import {console2} from "forge-std/Test.sol";
+
 import {CrossChainPool} from "./CrossChainPool.sol";
 import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
@@ -32,15 +34,15 @@ import {IRouterClient} from "ccip/interfaces/IRouterClient.sol";
 
 /**
  * @notice this contract deploy simultaneusly 2 pools on 2 networks
- * there should be a function to allow different networks
- * in inherit from chainlink ccip contracts todo, check which?
+ * todo there should be a function to allow different networks
+ * in inherit from chainlink ccip contracts
  * has an external function that anyone could call to deploy a crosschain pool pair
- *  todo should use clone patter to generate
  *  the deployer is the owner of the contract
- *  deployer should send feetoken to this contract!
+ *  deployer need to sen feetoken to this contract beforehand
  */
 contract PoolFactory is Ownable, CCIPReceiver {
     // errors
+    error PoolFactory__PoolDeploymentFailed();
     error PoolFactory__CrossChainDeploymentFailed(bytes call);
     // interfaces, libraries, contracts
     // Type declarations
@@ -48,18 +50,15 @@ contract PoolFactory is Ownable, CCIPReceiver {
     // State variables
     bytes32 private s_lastReceivedMessageId; // Store the last received messageId.
     bytes32 private s_lastSentMessageId; // Store the last received messageId.
-    string private s_lastReceivedCalldata; // Store the last received text.
+    bytes private s_lastReceivedData; // Store the last received text.
     address private immutable i_feeToken;
+    uint64 private constant DEPLOY_POOL_FUNCTION_ID = 1;
+    uint64 private constant SUCCESS_DEPLOY_FUNCTION_ID = 2;
+    mapping(uint64 => address[]) private s_deployedPools; //  maps chain selector to pool array
 
     // Events
     event PoolCreated(address indexed pool);
-    // The unique ID of the CCIP message.
-    // The chain selector of the source chain.
-    // The address of the sender from the source chain.
-    // The text that was received.
-    event MessageReceivedAndPoolDeployed(
-        bytes32 indexed messageId, uint64 indexed sourceChainSelector, address sender, address pool
-    );
+    event MessageReceived(bytes32 indexed messageId);
 
     // Modifiers
 
@@ -79,31 +78,58 @@ contract PoolFactory is Ownable, CCIPReceiver {
      * the contract should:
      *     1. deploy pool on current network
      *     2. send ccip message to factory on other networks
-     *      on other network, should be deployed the correspondent pool
-     *      the return
-     *  @param destinationChainSelector id of destination network
+     *      on the destination network, it should be deployed the correspondent pool
+     *  @param _receiverFactory the address of the receiver factory
+     *  @param _underlyingTokenOnSourceChain the underlying ERC20 token on current network
+     *  @param _underlyingTokenOnDestinationChain the underlying ERC20 token on destination network
+     *  @param _destinationChainSelector the chain selector from ccip of the destination network
      *  @param _poolName name of pool
      */
     function deployCCPools(
-        uint64 destinationChainSelector,
-        string memory _poolName,
-        address _underlyingToken,
-        address receiverFactory
+        address _receiverFactory,
+        address _underlyingTokenOnSourceChain,
+        address _underlyingTokenOnDestinationChain,
+        uint64 _destinationChainSelector,
+        string memory _poolName
     ) external returns (address) {
-        address deployedPool = deployPool(_underlyingToken, _poolName);
-        _sendCCipMessage(receiverFactory, destinationChainSelector);
+        (address deployedPool, bool success) = deployPool(
+            _underlyingTokenOnSourceChain, _poolName, _destinationChainSelector, _underlyingTokenOnDestinationChain
+        );
+        if (!success) {
+            revert PoolFactory__PoolDeploymentFailed();
+        }
+        _sendCCipMessageDeploy(
+            _receiverFactory,
+            _destinationChainSelector,
+            _underlyingTokenOnDestinationChain,
+            _poolName,
+            deployedPool,
+            _underlyingTokenOnSourceChain
+        );
         return deployedPool;
     }
 
     // public
     // todo check that only ccip can call it (or owner?)
-    function deployPool(address _underlyingToken, string memory _name) public returns (address poolAddress) {
+    function deployPool(
+        address _underlyingToken,
+        string memory _name,
+        uint64 _crossChainSelector,
+        address _underlyingTokenOnDestinationChain
+    ) public returns (address poolAddress, bool success) {
         // todo make necessary checks
-        IERC20 underlyingToken = IERC20(_underlyingToken);
-        CrossChainPool pool = new CrossChainPool(_underlyingToken, _name);
+        address router = getRouter();
+        CrossChainPool pool =
+            new CrossChainPool(_underlyingToken, _name, _crossChainSelector, router, _underlyingTokenOnDestinationChain);
         poolAddress = address(pool);
+        if (poolAddress == address(0)) {
+            success = false;
+        } else {
+            success = true;
+        }
+
         emit PoolCreated(poolAddress);
-        return poolAddress;
+        return (poolAddress, success);
     }
 
     // internal/ private
@@ -113,70 +139,144 @@ contract PoolFactory is Ownable, CCIPReceiver {
      * @notice deployer should send fee token to this contract
      * @param receiver the other factory on the other chain
      * @param destinationChainSelector the destination chain chainlink selector id
+     * @param _deployedPoolAddress the deployed pool to add as a allowed sender
      */
-    function _sendCCipMessage(
+    function _sendCCipMessageDeploy(
         address receiver,
         uint64 destinationChainSelector,
         address _underlyingOnOtherChain,
-        string memory _name
+        string memory _name,
+        address _deployedPoolAddress,
+        address _underlyingTokenOnSourceChain
     ) internal {
-        address router = getRouter();
-        bytes4 SELECTOR = bytes4(keccak256("deployPool(address,string)"));
+        address router = getRouter(); // it is the chainlink router for the current network
+
         Client.EVM2AnyMessage memory message = Client.EVM2AnyMessage({
-            receiver: abi.encode(receiver),
-            data: abi.encodeWithSelector(SELECTOR, _underlyingOnOtherChain, _name),
-            tokenAmounts: new Client.EVMTokenAmount[](0),
+            receiver: abi.encode(receiver), // receiver is the factory on destination chain
+            data: abi.encode(
+                DEPLOY_POOL_FUNCTION_ID, _underlyingOnOtherChain, _name, _deployedPoolAddress, _underlyingTokenOnSourceChain
+            ), //  the parameters to pass into deployPool
+            tokenAmounts: new Client.EVMTokenAmount[](0), // we are not passing tokens even tho we bridge bc we cool AF
             extraArgs: Client._argsToBytes(
                 // Additional arguments, setting gas limit
-                Client.EVMExtraArgsV1({gasLimit: 500_000})
+                Client.EVMExtraArgsV1({gasLimit: 3_000_000})
             ),
-            feeToken: i_feeToken
+            feeToken: i_feeToken // the token this contract will pay with
         });
 
         uint256 fee = IRouterClient(router).getFee(destinationChainSelector, message);
 
-        //deployer will send feetokens to this contract
         IERC20(i_feeToken).approve(address(router), fee);
 
         s_lastSentMessageId = IRouterClient(router).ccipSend(destinationChainSelector, message);
     }
 
     /**
+     * @notice send back a message to source chain when the deployment is successful
+     * @param _receiver the factory from the source chain
+     * @param _destinationChainSelector the ccip selector of the chain to send the message
+     * @param _deployedPoolAddress the pool address which was deployed
+     */
+    function _sendCCipMessageDeploySuccess(
+        address _receiver,
+        uint64 _destinationChainSelector,
+        address _deployedPoolAddress,
+        address _deployedPoolOnOtherChain
+    ) internal {
+        address router = getRouter(); // it is the chainlink router for the current network
+
+        Client.EVM2AnyMessage memory message = Client.EVM2AnyMessage({
+            receiver: abi.encode(_receiver), // receiver is the factory on destination chain
+            data: abi.encode(SUCCESS_DEPLOY_FUNCTION_ID, _deployedPoolAddress, "", _deployedPoolOnOtherChain, address(0)), //  the parameters to pass into deployPool
+            tokenAmounts: new Client.EVMTokenAmount[](0), // we are not passing tokens even tho we bridge bc we cool AF
+            extraArgs: Client._argsToBytes(
+                // Additional arguments, setting gas limit
+                Client.EVMExtraArgsV1({gasLimit: 3_000_000})
+            ),
+            feeToken: i_feeToken // the token this contract will pay with
+        });
+
+        uint256 fee = IRouterClient(router).getFee(_destinationChainSelector, message);
+
+        IERC20(i_feeToken).approve(address(router), fee);
+
+        s_lastSentMessageId = IRouterClient(router).ccipSend(_destinationChainSelector, message);
+    }
+
+    /**
      * @notice inherits form ccipReceiver and allow the contract to receive messages from other factories
-     *  todo it should deploy a new pool in the network where this contract is received
-     *  todo should allow just a smart contract to send messages here
-     *  todo finish implmentation
+     *  @notice it should deploy a new pool in the network where this contract is received
+     *  todo should allow just a smart contract to send messages here (make check)
+     *  should be ready to accept  3 type of msgs:
+     *     pool deployment request
+     *     succesfull deployment ack
+     *     failure on deployment
+     *
+     *  should trigger the send of 2 type of msgs, success (address of deployed pool) and fail msg (address(0))
+     *
+     *     selector defines the type of received msg
      */
     function _ccipReceive(Client.Any2EVMMessage memory any2EvmMessage) internal override {
-        s_lastReceivedMessageId = any2EvmMessage.messageId; // fetch the messageId
+        s_lastReceivedMessageId = any2EvmMessage.messageId;
 
         // those are the parameters
-        s_lastReceivedCalldata = any2EvmMessage.data;
-        // s_lastReceivedCalldata = abi.decode(any2EvmMessage.data, (address, string)); // abi-decoding of the sent text
+        s_lastReceivedData = any2EvmMessage.data; // maybe delete this line
 
-        (bool success, address deployedAddress) = address(this).call(any2EvmMessage.data);
+        (
+            uint64 functionID,
+            address underlyingTokenOrDeployedAddress,
+            string memory poolName,
+            address peerDeployedPool,
+            address underlyingTokenOnOtherChain
+        ) = abi.decode(any2EvmMessage.data, (uint64, address, string, address, address)); // abi-decoding of the sent text
 
-        if (!success) {
-            //should send back a message reverting the originating call from source chain --> no pool is created then
-            //todo send back a message to source chain
-            revert PoolFactory__CrossChainDeploymentFailed(any2EvmMessage.data);
+        if (functionID == DEPLOY_POOL_FUNCTION_ID) {
+            // todo find a good way to handle error here
+            (address deployedPool, bool success) = deployPool(
+                underlyingTokenOrDeployedAddress,
+                poolName,
+                any2EvmMessage.sourceChainSelector,
+                underlyingTokenOnOtherChain
+            );
+            if (success) {
+                //add the allowed sender -deployedPoolOnOtherChain- to the pool
+                CrossChainPool(deployedPool).addCrossChainSender(peerDeployedPool);
+                // send a fail msg
+                _sendCCipMessageDeploySuccess(
+                    abi.decode(any2EvmMessage.sender, (address)),
+                    any2EvmMessage.sourceChainSelector,
+                    deployedPool, // current chain
+                    peerDeployedPool // source chain
+                );
+            } else {
+                revert PoolFactory__PoolDeploymentFailed();
+            }
         }
 
-        // abi.encodeWithSignature("deployPool(address,string)", dataOrder)
+        // @todo send a fail msg if something goes wrong
 
-        emit MessageReceivedAndPoolDeployed(
-            any2EvmMessage.messageId,
-            any2EvmMessage.sourceChainSelector, // fetch the source chain identifier (aka selector)
-            abi.decode(any2EvmMessage.sender, (address)), // abi-decoding of the sender address,
-            deployedAddress
-        );
+        // here is when I receive the "ACK" telling that the deployment is ok
+        //receving it from destination chain
+        // @todo think about a better way to avoid using mapping uint -> address array
+        if (functionID == SUCCESS_DEPLOY_FUNCTION_ID) {
+            s_deployedPools[any2EvmMessage.sourceChainSelector].push(underlyingTokenOrDeployedAddress);
+
+            // in this function peerDeployedPool is actually the deployed pool in the current network
+            CrossChainPool(peerDeployedPool).addCrossChainSender(underlyingTokenOrDeployedAddress);
+        }
+
+        emit MessageReceived(any2EvmMessage.messageId);
     }
 
     // view fucntions
     /// @notice Fetches the details of the last received message.
     /// @return messageId The ID of the last received message.
-    /// @return text The last received text.
-    function getLastReceivedMessageDetails() external view returns (bytes32 messageId, string memory text) {
-        return (s_lastReceivedMessageId, s_lastReceivedCalldata);
+    /// @return data the data last received message.
+    function getLastReceivedMessageDetails() external view returns (bytes32 messageId, bytes memory data) {
+        return (s_lastReceivedMessageId, s_lastReceivedData);
+    }
+
+    function getALlDeployedPoolsForChainSelector(uint64 chainSelector) external view returns (address[] memory) {
+        return s_deployedPools[chainSelector];
     }
 }
